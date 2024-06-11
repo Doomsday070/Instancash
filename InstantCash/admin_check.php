caa1e34a783952d280ee2bf4eaffa516e989ffa0
Shuvojit Paul <167239502+Doomsday070@users.noreply.github.com>
2024-06-12 00:51:05 +0600

    admin_check.php

--- InstantCash/admin_check.php
+++ InstantCash/admin_check.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -11,9 +11,9 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if user is admin
-$phone = "01727618944"; // Example phone number
-$password = "232323";  // Example password
+
+$phone = "01727618944"; 
+$password = "232323";  
 
 $sql = "SELECT * FROM admin WHERE role_id = (SELECT login_id FROM role WHERE first_name = 'Admin' AND last_name = 'User')";
 $result = $conn->query($sql);
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $admin = $result->fetch_assoc();
     if ($admin['role_id'] == 1 && $admin['admin_role'] == 'Super Admin' && $phone == "01727618944" && $password == "232323") {
-        // Redirect to admin panel
+
         header("Location: admin_panel.php");
         exit();
     } else {
